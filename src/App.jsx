74d20a6d254298a081f0eccc0e6459046d1f62ed
xKitsune47 <franek.cy@wp.pl>
2025-03-29 17:20:55 +0100

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p className="font-semibold text-3xl">Whereas recognition</p>
+    </>
+  );
+}
+
+export default App;
